a4be0be74ce01b297387b4b03fc20d3417ef8015
king398 <mithilsalunkhe@outlook.com>
2021-09-18 21:17:11 +0530

    d

--- blending.py
+++ blending.py
@@ -3,17 +3,16 @@ import warnings
 import pandas as pd
 import numpy as np
 
-warnings.filterwarnings("ignore") 
+warnings.filterwarnings("ignore")
 
-
-sub1 = pd.read_csv("F:\Pycharm_projects\Gnet2\Sub\TPU\TEST_EfficientNetB7_512_1991._IMAGENET.csv").sort_values('id')
-sub2 = pd.read_csv('F:\Pycharm_projects\Gnet2\Sub\TPU\TEST_EfficientNetB7_512_1991_BETTER_NORM.csv').sort_values('id')
+sub1 = pd.read_csv(r"F:\Pycharm_projects\Gnet2\notebooks\blending.py").sort_values('id').reset_index(drop=True)
+sub2 = pd.read_csv('F:\Pycharm_projects\Gnet2\Sub\submission (1).csv').sort_values('id').reset_index(drop=True)
 preds1 = sub1.target
 preds2 = sub2.target
 
-
 sub = sub2.copy()
-sub.loc[:, 'target'] =preds1*0.5+preds2*0.5
+
+sub.loc[:, 'target'] = preds1 * 0.6 + preds2 * 0.4
 sub.to_csv(r'F:\Pycharm_projects\Gnet2\Sub/submission.csv', index=False)
 
 print(sub.head())
